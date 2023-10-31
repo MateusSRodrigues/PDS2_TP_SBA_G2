fd78fd320ef8fe7e86b3cdf70d6955fea688ff9d
marquesglaucia <109816494+marquesglaucia@users.noreply.github.com>
2023-10-31 17:29:16 -0400

    Update usuario.hpp

--- include/usuario.hpp
+++ include/usuario.hpp
@@ -1,1 +1,25 @@
+//
+// Created by rayda on 31/10/2023.
+//
 
+
+#ifndef ENTREGA1_USUARIO_H
+#define ENTREGA1_USUARIO_H
+
+
+#include <list>
+#include "Pessoa.h"
+
+class Usuario : public Pessoa{
+private:
+    list <Livro> livros_em_uso;
+public:
+    void reservar_livro(list <Livro> livros, list <string> reservas);   ///apaga o livro de livros e manda para reservas
+    void cancelar_reserva(list <string> reservas,list <Livro> livros);
+    void ver_multas();
+    void retirar_livro(Livro l,list <string> reservas);     ///apaga o livro de reservas e manda para livros_em_uso
+
+};
+
+
+#endif //ENTREGA1_USUARIO_H
