4d6fe2c86565f91fea39562a5bc96b08ce9006a2
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2023-10-31 21:50:37 -0300

    Update biblioteca.h

--- include/biblioteca.h
+++ include/biblioteca.h
@@ -1,1 +1,21 @@
+//
+// Created by rayda on 31/10/2023.
+//
+
+#ifndef ENTREGA1_BIBLIOTECA_H
+#define ENTREGA1_BIBLIOTECA_H
+
+
+class Biblioteca {
+ void emprestimo_livro();
+ void renovar_emrestimo();
+ void pesquisar_livro();
+ void reservar_livro();
+ void cancelar_reserva();
+ void ver_multa();
+ void pagar_multa();
+};
+
+
+#endif //ENTREGA1_BIBLIOTECA_H
 
