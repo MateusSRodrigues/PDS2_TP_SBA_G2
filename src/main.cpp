75f424774079534e268d8db2acde5dac269b5eb2
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2023-11-03 12:43:49 -0300

    Update funcao menu no main

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 #include <vector>
 #include <conio.h>
 #include <unistd.h>
+
 using namespace std;
-string menu(float valor_multa, vector<string>& opcao){
+string menu(vector<string>& opcao,  vector<string>& cabecalho){
         char n;
         int c;
-        cout << "Sua multa atual esta no valor de " << valor_multa << " deseja pagar agora?" << endl;
-        cout << "============================"<< endl << "->";
+        for (int i = 0; i < cabecalho.size(); i++) {
+            cout << cabecalho[i] << " ";
+        }
+        cout << endl << "============================"<< endl << "->";
         for (int i = 0; i < opcao.size(); i++) {
             cout << opcao[i] << endl;
         }
@@ -36,8 +39,10 @@ string menu(float valor_multa, vector<string>& opcao){
             for (int i = 0; i < opcao.size(); i++) {        ///loop imprime a lista de opçoes
 
                 if (i == 0) {                           /// para i=0 limpa tela e impime cabeçalho
-                    cout << "Sua multa atual esta no valor de " << valor_multa << " deseja pagar agora?" << endl;
-                    cout << "============================"<< endl;
+                    for (int i = 0; i < cabecalho.size(); i++) {
+                        cout << cabecalho[i] << " ";
+                    }
+                    cout << endl << "============================"<< endl;
                 }
                 if (i == c % opcao.size()) {        /// calcula e imprime seta
                     cout << "->";
@@ -52,28 +57,13 @@ string menu(float valor_multa, vector<string>& opcao){
         }
 }
 int main() {
-    vector <string> opca = {"sim", "nao","sempre"};
     float num;
     cin >> num;
-    cout << menu(num,opca);
-
-
-
-
-  //  std::list <int> lista = {1,2,3,4,5};
-   // std::list<int>::iterator it1;
-  //  for ( it1 = lista.begin(); it1 != lista.end(); it1++){
-    //    std::cout << *it1;
-   // }
-//std::cout<< " "<< std::endl;
-   // it1 = lista.begin();
-   // advance(it1,2);
-   // lista.erase(it1);
-
-   // for ( it1 = lista.begin(); it1 != lista.end(); it1++){
-  //      std::cout << *it1;
-   // }
-
-
+    string s = to_string(num);
+    s.resize(s.size()-4);
+    vector <string> opca = {"sim", "nao","sempre"};
+    vector<string> cabec = {"Sua multa atual esta no valor de ", s,"deseja pagar agora?"};
+    cout << menu(opca, cabec);
     return 0;
 }
+
