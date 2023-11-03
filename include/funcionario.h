2eaed70205e653499679a092cee86d6d6aea331f
mdsr2002 <mateusdesouzarodrigues@gmail.com>
2023-11-03 11:04:42 -0300

    Corrigindo erros de escrita

--- include/funcionario.h
+++ include/funcionario.h
@@ -7,16 +7,16 @@
 
 
 #include "Pessoa.h"
-#include "Usuario.h"
+#include "Usuario.hpp"
 
-class Funcionario: public Pessoa {e
+class Funcionario: public Pessoa {
 private:
 
 public:
     void informacoes_de_cadastro(Usuario u);
     void ver_multa(Usuario u);
     void pagar_multa(Usuario u, list<string> multas);
-    void entregar_livro_presencialmente(Livro l, Usuario u,);
+    void entregar_livro_presencialmente(Livro l, Usuario u);
     void cadastro_livro_novo(list<Livro> livros);
     void registrar_condicao_livro(Livro l);
     void renovar_emprestimo();
