9f69f84b523719267ec0a92c86492d2c7ec06ed3
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2023-11-25 18:55:24 -0300

    Update biblioteca.cpp

--- src/biblioteca.cpp
+++ src/biblioteca.cpp
@@ -11,32 +11,16 @@ void Biblioteca::criar_usuario(string n, string sen, string t, string id) {
     Usuario *user = new Usuario(n,sen,t,id);
     usuarios.push_back(user);
 }
-Usuario* Biblioteca::get_usuario(string identificacao) {
-    for (const auto &usuario: usuarios) {
-        if(usuario->get_identificacao() == identificacao){
-            return usuario;
-        }
-    }
-    return nullptr;
-}
 void Biblioteca::criar_funcionario(string n, string sen, string t, string id, string turno) {
     Funcionario *func = new Funcionario(n,sen,t,id,turno);
     funcionarios.push_back(func);
-}
-Funcionario* Biblioteca::get_funcionario(string identificacao){
-    for (const auto &funcionario: funcionarios) {
-        if(funcionario->get_identificacao() == identificacao){
-            return funcionario;
-        }
-    }
-    return nullptr;
 }
 void Biblioteca::adicionar_livro(Livro* novo_livro) {
     bool existe = 0 ;    /// Verificar se já existe uma lista com um livro com este nome
     for (auto& lista : todos_livros) {
         for (auto& livro_ptr : lista) {
             if (lista.front()->get_nome() == novo_livro->get_nome()) {   ///se ja existir um livro com esse nome, o livro criado é adicionado ao list dele.
-            existe = 1;
+                existe = 1;
                 novo_livro->set_link(lista.front()->get_link());
                 lista.push_back(novo_livro);           /// Adicionar o novo livro à lista existente
                 cout << "\033[32mEste livro foi adicionado a colecao com sucesso!\033[0m" << endl;
@@ -56,17 +40,17 @@ void Biblioteca::adicionar_livro(Livro* novo_livro) {
         string link = "https://www.sist.biblio.avac.com/pdf_livro/" + novo_livro->get_nome() + linkGerado;
         novo_livro->set_link(link);
     }
-        list<Livro *> nova_lista;
-        nova_lista.push_back(novo_livro);
-        todos_livros.push_back(nova_lista);
+    list<Livro *> nova_lista;
+    nova_lista.push_back(novo_livro);
+    todos_livros.push_back(nova_lista);
     cout << "\033[32mEste livro foi adicionado ao acervo de livros da biblioteca com sucesso!\033[0m" << endl;
-        auto compararNomeDoPrimeiroLivro = [](const std::list<Livro*>& lista1, const std::list<Livro*>& lista2) {        /// comparar o nome dos primeiros livros
-            if (!lista1.empty() && !lista2.empty()) {
-                return lista1.front()->get_nome() < lista2.front()->get_nome();
-            }
-            return false; // ou defina a lógica para lidar com listas vazias, se necessário
-        };
-        todos_livros.sort(compararNomeDoPrimeiroLivro);     /// Ordenar a lista de listas com base no comparador personalizado
+    auto compararNomeDoPrimeiroLivro = [](const std::list<Livro*>& lista1, const std::list<Livro*>& lista2) {        /// comparar o nome dos primeiros livros
+        if (!lista1.empty() && !lista2.empty()) {
+            return lista1.front()->get_nome() < lista2.front()->get_nome();
+        }
+        return false; // ou defina a lógica para lidar com listas vazias, se necessário
+    };
+    todos_livros.sort(compararNomeDoPrimeiroLivro);     /// Ordenar a lista de listas com base no comparador personalizado
 }
 void Biblioteca::remover_livro(string id_livro) {
     for (auto it_lista = todos_livros.begin(); it_lista != todos_livros.end(); ++it_lista) {
@@ -83,5 +67,3 @@ void Biblioteca::remover_livro(string id_livro) {
             }
         }
     }
-    throw runtime_error("\033[1;31mLivro nao encontrado no acervo!\033[0m");
-}
