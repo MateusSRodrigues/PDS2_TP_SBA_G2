f0d9b92607826a7d928babadd5eacc2b66f9fbd8
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2023-11-27 09:31:58 -0300

    Update biblioteca.h
    get usuario

--- include/biblioteca.h
+++ include/biblioteca.h
@@ -26,12 +26,15 @@ private:
 
 public:
     bool pedido_renovar_emprestimo_efetuado(string nome_pedido, string matricula);
-    void criar_usuario(string n, string sen, string t, string id);
+     void criar_usuario(string n, string sen, string t, string id);
     void criar_funcionario(string n, string sen, string t, string id, string turno);
+    void adicionar_livro(Livro* novo_livro );
     void criar_pedido_reserva(string matricula, Livro *l);
     void criar_pedido_renovar_emprestimo(string matricula, Livro *l);
-    void adicionar_livro(Livro* novo_livro );
     void remover_livro(string id_livro);
+    void deletar_pedido_reserva(string identificaçao, string nome_livro);
+    void deletar_pedido_renovar_emprestimo(string identificaçao, string nome_livro);
+    Usuario* get_usuario(string identificacao);
 };
 
 
